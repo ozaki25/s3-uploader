d9794dca7f3e87e2a878f7381b5ecf5398b3537e
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-01-25 17:19:16 +0900

    create s3 url function

--- index.js
+++ index.js
@@ -4,27 +4,49 @@ const aws = require('aws-sdk');
 
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-const roleName = 's3Upload';
+const roleId = process.env.ROLE_ID;
+const roleName = process.env.ROLE_NAME;
+const Bucket = process.env.BUCKET;
 
 const app = express();
 
-const param = {
-  RoleArn: `arn:aws:iam::[ユーザの12桁ID]:role/${roleName}`,
+const stsParams = {
+  RoleArn: `arn:aws:iam::${roleId}:role/${roleName}`,
   RoleSessionName: `session_${roleName}`,
 };
+
+const s3Params = ({ Key, ContentType }) => ({
+  Bucket,
+  Key,
+  ContentType,
+});
 
 const sts = new aws.STS({
   accessKeyId,
   secretAccessKey,
 });
 
-const s3 = new aws.S3({
-  accessKeyId,
-  secretAccessKey,
-});
+app.get('/', (req, res) => {
+  const {
+    query: { filename, mimetype },
+  } = req;
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+  sts.assumeRole(stsParams, (error, data) => {
+    if (error) {
+      console.log({ data, error });
+      return;
+    }
+    const {
+      Credentials: { AccessKeyId, SecretAccessKey, SessionToken },
+    } = data;
+    const s3 = new aws.S3({
+      accessKeyId: AccessKeyId,
+      secretAccessKey: SecretAccessKey,
+      sessionToken: SessionToken,
+    });
+    const url = s3.getSignedUrl('putObject', s3Params({ Key: filename, ContentType: mimetype }));
+    res.send({ url });
+  });
 });
 
 module.exports.handler = serverless(app);
