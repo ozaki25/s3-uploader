5f29661f0f0c6ed286dff279b36707e6d0a562f2
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-25 23:46:53 +0900

    add example

---
+++ examples/index.js
@@ -0,0 +1,55 @@
+const lambdaUrl = ''; // your api url
+
+const template = `
+<div>
+  <input type="file" name="file" />
+  <button type="button">Upload</button>
+</div>
+`;
+
+class XUploader extends HTMLElement {
+  connectedCallback() {
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = template;
+    this.shadowRoot.querySelector('button').addEventListener('click', this.onClick);
+  }
+
+  getUploadUrl = ({ url, filename, mimetype }) =>
+    fetch(`${url}?filename=${filename}&mimetype=${mimetype}`);
+
+  putFile = ({ url, file, type }) =>
+    fetch(url, { method: 'PUT', headers: { 'Content-Type': type }, body: file });
+
+  appendImage = url => {
+    const img = document.createElement('img');
+    img.setAttribute('src', url);
+    this.shadowRoot.appendChild(img);
+  };
+
+  onClick = async e => {
+    e.preventDefault();
+    const inputFile = this.shadowRoot.querySelector('input[name="file"]');
+    const file = inputFile.files[0];
+    const { name, type } = file;
+
+    try {
+      const getUploadUrlResponse = await this.getUploadUrl({
+        url: lambdaUrl,
+        filename: name,
+        mimetype: type,
+      });
+      const { url, bucket } = await getUploadUrlResponse.json();
+      const putFileResponse = await this.putFile({ url, file, type });
+      if (putFileResponse.ok) {
+        console.log(`https://s3-ap-northeast-1.amazonaws.com/${bucket}/${name}`);
+        this.appendImage(`https://s3-ap-northeast-1.amazonaws.com/${bucket}/${name}`);
+      } else {
+        console.log(putFileResponse);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+}
+
+window.customElements.define('x-uploader', XUploader);
